0a388a45efd8dfb8498afe418ffaaf48c1c4dfe5
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-16 22:01:40 +0600

    setup Redux store with persisted state and API middleware

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/redux/store.ts
@@ -0,0 +1,51 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { baseApi } from "./api/baseApi";
+import storage from "redux-persist/lib/storage";
+import { cartSlice } from "./features/cart/cartSlice";
+import { auth } from "./features/auth/authSlice";
+import { productSlice } from "./features/products/productSlice";
+
+// Root Reducer
+const rootReducer = combineReducers({
+  [baseApi.reducerPath]: baseApi.reducer,
+  auth: auth.reducer,
+  cart: cartSlice.reducer,
+  product: productSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "cart", "product"], // Only persist selected reducers
+};
+
+// Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
+});
+
+// Types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Redux Persist Store
+export const persistor = persistStore(store);
