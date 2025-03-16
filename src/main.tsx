6bb94633e746825a3653b9bb77ed7b66ef8a1ee7
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-16 22:11:46 +0600

    setup router with App and Home components

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import router from "./routes/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/routes/routes.tsx
@@ -0,0 +1,17 @@
+import App from "@/App";
+import Home from "@/pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+export default router;
