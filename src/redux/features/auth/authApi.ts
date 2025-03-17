5a6585ae159d7826d480e4b70c2a7120b0e4de20
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-17 11:30:49 +0600

    add authentication API with login mutation

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,16 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // login
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
