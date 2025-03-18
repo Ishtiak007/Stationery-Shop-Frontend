0fcb43aae0a285c46f8fbb4f3e9107ce3a219651
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-18 13:56:42 +0600

    add user registration form with validation and API integration

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,203 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// @typescript-eslint/no-explicit-any
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu, X, ShoppingCart, User } from "lucide-react";
+
+import { BiLogOut } from "react-icons/bi";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logOut, useCurrentToken } from "@/redux/features/auth/authSlice";
+import { useGetMeQuery } from "@/redux/features/auth/authApi";
+import { verifyToken } from "@/utils/verifyToken";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const token = useAppSelector(useCurrentToken);
+  const cartData = useAppSelector((state) => state?.cart);
+  const dispacth = useAppDispatch();
+
+  const { data: myData } = useGetMeQuery(undefined);
+  const initials: string = myData?.data?.name as string;
+
+  let user: any;
+  if (token) {
+    user = verifyToken(token);
+  }
+
+  const cartCount = cartData?.items[0]?.quantity || 0;
+
+  const dashboardLink =
+    user?.role === "admin"
+      ? "/dashboard"
+      : user?.role === "user"
+      ? "/dashboard"
+      : "/";
+
+  const handleLogOut = () => {
+    dispacth(logOut());
+  };
+
+  const [position, setPosition] = useState("bottom");
+  return (
+    <nav className="bg-gray-200 dark:bg-gray-900 text-black dark:text-white  font-orbitron border-2 border-radius-2xl  overflow-hidden shadow-lg z-10 w-full">
+      {/* Container */}
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        {/* Logo */}
+        <Link to="/" className="flex items-center">
+          <span className="text-black  font-orbitron text-[8px] md:text:sm lg:text-xl font-bold ml-2">
+            Stationery Shop
+          </span>
+        </Link>
+
+        {/* Desktop Search Bar */}
+        <div className="hidden lg:flex items-center w-1/3">
+          <div>
+            <div className="space-x-2 p-2 md:flex items-center justify-center text-black">
+              <button className="bg-blue-200">
+                <Link to={"/"}>Home</Link>
+              </button>
+              <button className="bg-blue-200">
+                <Link to={"/all-product"}>Products</Link>
+              </button>
+              <button className="bg-blue-200">
+                <Link to={"/about"}>About</Link>
+              </button>
+            </div>
+          </div>
+          {/* <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600" /> */}
+        </div>
+
+        {/* Icons and Menu */}
+        <div className="flex items-center md:space-x-4">
+          <Link to="/cart" className="relative">
+            <ShoppingCart size={20} />
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+              {cartCount}
+            </span>
+          </Link>
+
+          {user ? (
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant={"default"}>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <User size={28} />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p className="font-orbitron font-bold uppercase">
+                            {initials}
+                          </p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56 font-orbitron">
+                  {/* <DropdownMenuLabel className="mt-2">Panel Position</DropdownMenuLabel> */}
+                  <DropdownMenuSeparator />
+                  <DropdownMenuRadioGroup
+                    value={position}
+                    onValueChange={setPosition}
+                  >
+                    <DropdownMenuRadioItem value="bottom">
+                      {/* dashboard */}
+
+                      <Link to={dashboardLink}>
+                        <Button variant={"outline"}>
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger>
+                                <MdOutlineSpaceDashboard className="text-blue-700" />
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p className="font-bold font-orbitron">
+                                  Dashboard
+                                </p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </Button>
+                      </Link>
+                    </DropdownMenuRadioItem>
+
+                    <DropdownMenuRadioItem value="right">
+                      {/* log out */}
+                      <Button onClick={handleLogOut} variant={"outline"}>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <BiLogOut className="text-blue-800" />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="font-bold font-orbitron">Log Out</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </Button>
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          ) : (
+            <Link to="/register">
+              <User size={24} />
+            </Link>
+          )}
+
+          {/* Mobile Menu Button */}
+          <Button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={28} /> : <Menu size={28} />}
+          </Button>
+        </div>
+      </div>
+
+      {/* mobile */}
+      {isOpen && (
+        <div className="lg:hidden bg-white shadow-md">
+          <Link
+            to="/"
+            className="block py-2 px-4 text-[10px] hover:bg-gray-200"
+          >
+            Home
+          </Link>
+          <Link
+            to="/all-product"
+            className="block py-2 px-4 text-[10px] hover:bg-gray-200"
+          >
+            Products
+          </Link>
+          <Link
+            to="/about"
+            className="block py-2 px-4 text-[10px] hover:bg-gray-200"
+          >
+            About
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
