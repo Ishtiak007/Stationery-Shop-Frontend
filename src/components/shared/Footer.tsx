2b43bd3c91fb4fbf74d78db1c08eeb9443852f30
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-18 11:55:12 +0600

    Footer section updated

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -19,66 +19,80 @@ const Footer = () => {
         <div>
           <div className="flex justify-center items-center">
             <img className="size-20" src={logo} alt="logo" />
-            <p className="text-3xl font-semibold">Stationery Shop</p>
+            <p className="text-xl font-semibold">Stationery Shop</p>
           </div>
 
-          <p className=" w-[80%] lg:w-[80%] mt-3">
-            Design amazing digital experiences that create more happy in the
-            world.
+          <p className=" w-[80%] lg:w-[80%] mt-3 text-center">
+            Your One-Stop Shop for All Stationery Needs
           </p>
         </div>
         <div>
           <div className="font-bold mb-6">Services</div>
           <div className="flex flex-col gap-4">
             <a href="" className="text-sm hover:underline">
-              Web Design
+              Free Delivery
             </a>
             <a href="" className="text-sm hover:underline">
-              Web Development
+              Bulk Order Discounts
             </a>
             <a href="" className="text-sm hover:underline">
               {" "}
-              Science
+              Custom Printing & Branding
             </a>
             <a href="" className="text-sm hover:underline">
-              Digital Marketing
+              Gift Wrapping Services
+            </a>
+            <a href="" className="text-sm hover:underline">
+              School & Office Supply Packages
             </a>
           </div>
         </div>
+        {/* .......Customer Support....... */}
         <div>
-          <div className="font-bold mb-6">Company</div>
+          <div className="font-bold mb-6">Customer Support</div>
           <div className="flex flex-col gap-4">
             <a href="" className="text-sm hover:underline">
-              Privacy Policy
+              Help Center
             </a>
             <a href="" className="text-sm hover:underline">
-              Sitemap
+              Return & Refund Policy
             </a>
             <a href="" className="text-sm hover:underline">
-              Careers
+              {" "}
+              Shipping & Delivery Information
             </a>
             <a href="" className="text-sm hover:underline">
               Terms & Conditions
             </a>
+            <a href="" className="text-sm hover:underline">
+              Privacy Policy
+            </a>
           </div>
         </div>
+
         <div>
-          <div className="font-bold mb-6">Follow us</div>
-          <div className="text-sm mb-4">ishtiakahmed18899@gmail.com</div>
-          <div className="text-sm">+8801521742729</div>
-          <div className="flex gap-4 mt-4">
-            <a href="" className="hover:scale-110 text-xl">
-              <BsFacebook />
-            </a>
-            <a href="" className="hover:scale-110 text-xl">
-              <BsInstagram />
-            </a>
-            <a href="" className="hover:scale-110 text-xl">
-              <BsTwitter />
-            </a>
-            <a href="" className="hover:scale-110 text-xl">
-              <BsPinterest />
-            </a>
+          <div className="font-bold mb-3">Contact Information</div>
+          <h2 className="text-sm mb-4">ishtiakahmed18899@gmail.com</h2>
+          <h2 className="text-sm">+8801521742729</h2>
+          <div>
+            <div className="font-bold my-6">
+              <h1>Follow Us (Social Media)</h1>
+
+              <div className="flex gap-4 mt-4">
+                <a href="" className="hover:scale-110 text-xl">
+                  <BsFacebook />
+                </a>
+                <a href="" className="hover:scale-110 text-xl">
+                  <BsInstagram />
+                </a>
+                <a href="" className="hover:scale-110 text-xl">
+                  <BsTwitter />
+                </a>
+                <a href="" className="hover:scale-110 text-xl">
+                  <BsPinterest />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
