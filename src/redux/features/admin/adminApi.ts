0d2106ac424fde36272f8e35da08ce8bc70f88d7
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-19 14:47:49 +0600

    add user status update mutation in admin API

---
+++ src/redux/features/admin/adminApi.ts
@@ -0,0 +1,16 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const adminApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    userStatusUpdate: builder.mutation({
+      query: (data) => ({
+        url: "/users/update-profile",
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const { useUserStatusUpdateMutation } = adminApi;
