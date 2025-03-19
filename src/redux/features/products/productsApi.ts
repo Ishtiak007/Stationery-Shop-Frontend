49bfff8b118742475ad98344a4782a0d3cae1e73
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-19 13:33:36 +0600

    add RTK Query endpoint for fetching all products with query params

---
+++ src/redux/features/products/productsApi.ts
@@ -0,0 +1,33 @@
+import { baseApi } from "@/redux/api/baseApi";
+import { TQueryParam, TReponseRedux } from "@/types/globalTypes";
+import { TProducts } from "@/types/productTypes";
+
+const productsApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    //all products
+    getAllProducts: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/products/",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["product"],
+      transformResponse: (response: TReponseRedux<{ result: TProducts[] }>) => {
+        return {
+          data: response?.data?.result,
+          meta: response?.data,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetAllProductsQuery } = productsApi;
