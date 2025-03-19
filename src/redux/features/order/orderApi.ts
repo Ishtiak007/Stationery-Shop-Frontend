8d4fade9062b4ae8275e626a57ae56836a9bd3c5
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-19 14:12:47 +0600

    implement order API with create and fetch orders functionality

---
+++ src/redux/features/order/orderApi.ts
@@ -0,0 +1,32 @@
+import { baseApi } from "@/redux/api/baseApi";
+import { TReponseRedux } from "@/types/globalTypes";
+import { Order } from "@/types/orderDetailsTypes";
+
+const orderApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (userInfo) => ({
+        url: "/orders",
+        method: "POST",
+        body: userInfo,
+      }),
+      invalidatesTags: ["orders"],
+    }),
+    getOrders: builder.query({
+      query: () => {
+        return {
+          url: "/orders",
+          method: "GET",
+        };
+      },
+      providesTags: ["orders"],
+      transformResponse: (response: TReponseRedux<{ result: Order[] }>) => {
+        return {
+          data: response?.data,
+          meta: response?.data,
+        };
+      },
+    }),
+  }),
+});
+export const { useCreateOrderMutation, useGetOrdersQuery } = orderApi;
