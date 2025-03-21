20149f5d3137d2f0bc619b23833da29fbb178e5c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-21 15:59:34 +0600

    ensure correct product selection in ProductDetails

---
+++ src/pages/AllProducts/ProductDetails.tsx
@@ -0,0 +1,24 @@
+/*eslint@typescript-eslint/no-explicit-any*/
+import { useGetAllProductsQuery } from "@/redux/features/products/productsApi";
+
+import { TProducts } from "@/types/productTypes";
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+  const { data: productData } = useGetAllProductsQuery(undefined);
+  const { id } = useParams();
+
+  const product = productData?.data?.filter(
+    (product: TProducts) => product._id === id
+  );
+
+  return (
+    <div className="container mx-auto mt-7 overflow-hidden mb-8">
+      {product?.map((details) => {
+        return <ProductDetailsCard product={details} key={details._id} />;
+      })}
+    </div>
+  );
+};
+
+export default ProductDetails;
